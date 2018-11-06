e8bc5673ec3759c4097376d0c5719aaad2a51a0b
Jesper Bergström <jesper.b96@gmail.com>
2018-11-06 04:31:49 +0100

    Set window to appropriate size

--- src/editor/controller/MainWindowController.java
+++ src/editor/controller/MainWindowController.java
@@ -11,6 +11,8 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -19,6 +21,8 @@ public class MainWindowController implements LevelEditorController {
 	
 	@FXML public MenuItem menuFileNew;
 	@FXML public Canvas canvas;
+	@FXML public ScrollPane scrollPane;
+	@FXML public AnchorPane anchorPane;
 	
 	private LevelSettings levelSettings;
 	private int[][] tileMap;
@@ -26,6 +30,11 @@ public class MainWindowController implements LevelEditorController {
 	private final int TILE_SIZE = 50;
 	
 	@FXML public void initialize() {
+		AnchorPane.setTopAnchor(scrollPane, 0.0);
+		AnchorPane.setBottomAnchor(scrollPane, 0.0);
+		AnchorPane.setLeftAnchor(scrollPane, 0.0);
+		AnchorPane.setRightAnchor(scrollPane, 0.0);
+		
 		menuFileNew.setOnAction(e -> {
 			Parent root;
 			URI location = new File("res/" + LEVEL_SETTINGS_WINDOW).toURI();
@@ -53,11 +62,21 @@ public class MainWindowController implements LevelEditorController {
 	public void setNewLevel(LevelSettings levelSettings) {
 		this.levelSettings = levelSettings;
 		this.tileMap = new int[levelSettings.width][levelSettings.height];
-		this.canvas.setWidth(levelSettings.width * TILE_SIZE);
-		this.canvas.setHeight(levelSettings.height * TILE_SIZE);
+		this.canvas.setWidth(levelSettings.width * TILE_SIZE + 15);
+		this.canvas.setHeight(levelSettings.height * TILE_SIZE + 15);
 		Stage stage = (Stage) this.canvas.getScene().getWindow();
-		stage.setWidth(this.canvas.getWidth() + 7);
-		stage.setHeight(this.canvas.getHeight() + 55);
+		int width = (int) (this.canvas.getWidth() + 7);
+		int height = (int) (this.canvas.getHeight() + 55);
+		
+		// TODO: Get size of user screen.
+		if (width > 1600) {
+			width = 1600;
+		}
+		if (height > 900) {
+			height = 900;
+		}
+		stage.setWidth(width);
+		stage.setHeight(height);
 		stage.setResizable(false);
 		
 		GraphicsContext g = this.canvas.getGraphicsContext2D();
